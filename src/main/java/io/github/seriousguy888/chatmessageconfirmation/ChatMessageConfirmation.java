bf6ee7f74c9aed72ed2a89ae695284c17205af28
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-04-24 16:39:20 -0400

    only send chat messages with bypass prefix

--- src/main/java/io/github/seriousguy888/chatmessageconfirmation/ChatMessageConfirmation.java
+++ src/main/java/io/github/seriousguy888/chatmessageconfirmation/ChatMessageConfirmation.java
@@ -2,23 +2,28 @@ package io.github.seriousguy888.chatmessageconfirmation;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ChatMessageConfirmation implements ModInitializer {
-	public static final String MOD_ID = "chat-message-confirmation";
+    public static final String MOD_ID = "chat-message-confirmation";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    private static final String BYPASS_PREFIX = "#";
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        ClientSendMessageEvents.ALLOW_CHAT.register(message -> {
+            return message.startsWith(BYPASS_PREFIX);
+        });
 
-		LOGGER.info("Hello Fabric world!");
-	}
+        ClientSendMessageEvents.MODIFY_CHAT.register(message -> {
+            if (message.startsWith(BYPASS_PREFIX)) {
+                return message.substring(BYPASS_PREFIX.length());
+            } else {
+                return message;
+            }
+        });
+    }
 }
