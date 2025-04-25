2a26e7a3831158c4f4c0a375ff638aea6748965f
billzonium trioxide <38299346+SeriousGuy888@users.noreply.github.com>
2025-04-25 13:53:59 -0400

    added lang file

--- src/main/java/io/github/seriousguy888/chatmessageconfirmation/gui/ConfirmationGui.java
+++ src/main/java/io/github/seriousguy888/chatmessageconfirmation/gui/ConfirmationGui.java
@@ -8,6 +8,11 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 public class ConfirmationGui extends LightweightGuiDescription {
+    private static final String ARE_YOU_SURE_CHAT_MESSAGE = "gui.chat_message_confirmation.are_you_sure.chat_message";
+    private static final String ARE_YOU_SURE_COMMAND = "gui.chat_message_confirmation.are_you_sure.command";
+    private static final String BUTTON_CONFIRM = "gui.chat_message_confirmation.button.confirm";
+    private static final String BUTTON_CANCEL = "gui.chat_message_confirmation.button.cancel";
+
     public ConfirmationGui(String messageContents) {
         int cellSize = 18;
         int gap = 2;
@@ -25,18 +30,18 @@ public class ConfirmationGui extends LightweightGuiDescription {
         root.setGaps(gap, gap);
         root.setInsets(Insets.ROOT_PANEL);
 
-        WLabel label = new WLabel(Text.literal("Are you sure you want to send this message?"));
+        WLabel label = new WLabel(Text.translatable(ARE_YOU_SURE_CHAT_MESSAGE));
         root.add(label, 0, 0, gridWidth, 1);
 
         WText textField = new WText(Text.literal(messageContents)).setColor(quoteColour, quoteColourDarkMode);
         root.add(textField, 1, 1, gridWidth - 2, gridHeight - 2);
 
         MinecraftClient client = MinecraftClient.getInstance();
-        WButton yesButton = new WButton(Text.literal("Confirm")).setOnClick(() -> {
+        WButton yesButton = new WButton(Text.translatable(BUTTON_CONFIRM)).setOnClick(() -> {
             ChatMessageConfirmationMod.getInstance().sendPendingMessage(messageContents);
             client.send(() -> client.setScreen(null));
         });
-        WButton noButton = new WButton(Text.translatable("gui.cancel")).setOnClick(() -> {
+        WButton noButton = new WButton(Text.translatable(BUTTON_CANCEL)).setOnClick(() -> {
             client.send(() -> client.setScreen(null));
         });
         root.add(yesButton, 0, gridHeight - 1, gridWidth / 2, 1);
